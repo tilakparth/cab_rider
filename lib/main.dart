199446d23c9a875ff437bef15038874b5d615dfe
tilakparth <tilakparth123@gmail.com>
2021-07-28 23:39:55 +0530

    firebase setup completed and images and fonts added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:cab_rider/screens/mainpage.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final FirebaseApp app = await Firebase.initializeApp(
+      name: 'db2',
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyBF-IpjTPUeYh09Eo8c_Yp13LUI0d-O3Vs",
+        appId: "1:536217535308:android:9f79f09aa75121c55b05e9",
+        messagingSenderId: "536217535308",
+        projectId: "geetaxi-6903e",
+        databaseURL: "https://geetaxi-6903e-default-rtdb.firebaseio.com",
+      ));
   runApp(MyApp());
 }
 
@@ -11,6 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        fontFamily: 'Brand-Regular',
         primarySwatch: Colors.blue,
       ),
       home: MainPage(),
--- lib/screens/mainpage.dart
+++ lib/screens/mainpage.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -13,6 +14,19 @@ class _MainPageState extends State<MainPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Main Page'),
+      ),
+      body: Center(
+        child: MaterialButton(
+          height: 50.0,
+          minWidth: 300.0,
+          color: Colors.green,
+          child: Text('Test Connection'),
+          onPressed: () {
+            DatabaseReference dbref =
+                FirebaseDatabase.instance.reference().child('Test');
+            dbref.set('IsConnected');
+          },
+        ),
       ),
     );
   }
