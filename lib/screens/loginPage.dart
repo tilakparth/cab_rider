6e1b10a4ea704f264b267b9364a4ebabfba7e068
tilakparth <tilakparth123@gmail.com>
2021-07-29 13:35:35 +0530

    Firebase authentication integrated with login page

--- lib/screens/loginPage.dart
+++ lib/screens/loginPage.dart
@@ -1,10 +1,75 @@
+import 'package:cab_rider/screens/mainpage.dart';
 import 'package:cab_rider/screens/registrationpage.dart';
+import 'package:cab_rider/widgets/ProgressDialog.dart';
+import 'package:connectivity/connectivity.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:cab_rider/widgets/TaxiButton.dart';
 import 'package:cab_rider/brand_colors.dart';
+import 'package:flutter/services.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   static const String id = "login";
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  void showSnackBar(title, context) {
+    final SnackBar snackBar = SnackBar(
+      content: Text(
+        title,
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 15),
+      ),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  var emailController = TextEditingController();
+
+  var passwordController = TextEditingController();
+
+  void login() async {
+    //show please wait dialog
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) => ProgressDialog(
+        status: 'Logging you in',
+      ),
+    );
+
+    final User? user = (await _auth
+            .signInWithEmailAndPassword(
+      email: emailController.text,
+      password: passwordController.text,
+    )
+            .catchError((ex) {
+      //check error and display message
+      Navigator.pop(context);
+      PlatformException thisEx = ex;
+      showSnackBar(thisEx.message, context);
+    }))
+        .user;
+
+    if (user != null) {
+      // verify login
+      DatabaseReference userRef =
+          FirebaseDatabase.instance.reference().child('users/${user.uid}');
+      userRef.once().then((DataSnapshot snapshot) {
+        if (snapshot.value != null) {
+          Navigator.pushNamedAndRemoveUntil(
+              context, MainPage.id, (route) => false);
+        }
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +104,7 @@ class LoginPage extends StatelessWidget {
                     padding: EdgeInsets.all(20.0),
                     child: Column(children: <Widget>[
                       TextField(
-                        // controller: emailController,
+                        controller: emailController,
                         keyboardType: TextInputType.emailAddress,
                         decoration: InputDecoration(
                             labelText: 'Email address',
@@ -54,7 +119,7 @@ class LoginPage extends StatelessWidget {
                         height: 10,
                       ),
                       TextField(
-                        // controller: passwordController,
+                        controller: passwordController,
                         obscureText: true,
                         decoration: InputDecoration(
                             labelText: 'Password',
@@ -72,7 +137,30 @@ class LoginPage extends StatelessWidget {
                         title: 'LOGIN',
                         color: BrandColors.colorGreen,
                         // color: BrandColors.colorGreen,
-                        onPressed: () {},
+                        onPressed: () async {
+                          //check network availability
+
+                          var connectivityResult =
+                              await Connectivity().checkConnectivity();
+                          if (connectivityResult != ConnectivityResult.mobile &&
+                              connectivityResult != ConnectivityResult.wifi) {
+                            showSnackBar('No internet connectivity', context);
+                            return;
+                          }
+
+                          if (!emailController.text.contains('@')) {
+                            showSnackBar(
+                                'Please enter a valid email address', context);
+                            return;
+                          }
+
+                          if (passwordController.text.length < 8) {
+                            showSnackBar(
+                                'Please enter a valid password', context);
+                            return;
+                          }
+                          login();
+                        },
                       ),
                       FlatButton(
                           onPressed: () {
