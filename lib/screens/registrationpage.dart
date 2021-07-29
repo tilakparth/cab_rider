1cb218197b7434db9d7e718fe41463f03c8a9d0a
tilakparth <tilakparth123@gmail.com>
2021-07-29 13:02:43 +0530

    Firebase authentication completed for registration page

--- lib/screens/registrationpage.dart
+++ lib/screens/registrationpage.dart
@@ -1,7 +1,13 @@
 import 'package:cab_rider/screens/loginPage.dart';
+import 'package:cab_rider/screens/mainpage.dart';
+import 'package:cab_rider/widgets/ProgressDialog.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:cab_rider/brand_colors.dart';
 import 'package:cab_rider/widgets/TaxiButton.dart';
+import 'package:flutter/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:connectivity/connectivity.dart';
 
 class RegistrationPage extends StatefulWidget {
   static const String id = "register";
@@ -15,6 +21,62 @@ class _RegistrationPageState extends State<RegistrationPage> {
   var phoneController = TextEditingController();
   var emailController = TextEditingController();
   var passwordController = TextEditingController();
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  void showSnackBar(title, context) {
+    final SnackBar snackBar = SnackBar(
+      content: Text(
+        title,
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 15),
+      ),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void registerUser() async {
+    //show please wait dialog
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) => ProgressDialog(
+        status: 'Registering you...',
+      ),
+    );
+
+    final User? user = (await _auth
+            .createUserWithEmailAndPassword(
+      email: emailController.text,
+      password: passwordController.text,
+    )
+            .catchError((ex) {
+      //check error and display message
+      Navigator.pop(context);
+      PlatformException thisEx = ex;
+      showSnackBar(thisEx.message, context);
+    }))
+        .user;
+
+    Navigator.pop(context);
+    // check if user registration is successful
+    if (user != null) {
+      DatabaseReference newUserRef =
+          FirebaseDatabase.instance.reference().child('users/${user.uid}');
+
+      //Prepare data to be saved on users table
+      Map userMap = {
+        'fullname': fullNameController.text,
+        'email': emailController.text,
+        'phone': phoneController.text,
+      };
+
+      newUserRef.set(userMap);
+
+      //Take the user to the mainPage
+      Navigator.pushNamedAndRemoveUntil(context, MainPage.id, (route) => false);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +110,7 @@ class _RegistrationPageState extends State<RegistrationPage> {
                   padding: EdgeInsets.all(20.0),
                   child: Column(
                     children: <Widget>[
-                      // Fullname
+                      // Name
                       TextField(
                         controller: fullNameController,
                         keyboardType: TextInputType.text,
@@ -61,11 +123,9 @@ class _RegistrationPageState extends State<RegistrationPage> {
                                 TextStyle(color: Colors.grey, fontSize: 10.0)),
                         style: TextStyle(fontSize: 14),
                       ),
-
                       SizedBox(
                         height: 10,
                       ),
-
                       // Email Address
                       TextField(
                         controller: emailController,
@@ -79,11 +139,9 @@ class _RegistrationPageState extends State<RegistrationPage> {
                                 TextStyle(color: Colors.grey, fontSize: 10.0)),
                         style: TextStyle(fontSize: 14),
                       ),
-
                       SizedBox(
                         height: 10,
                       ),
-
                       // Phone
                       TextField(
                         controller: phoneController,
@@ -123,7 +181,44 @@ class _RegistrationPageState extends State<RegistrationPage> {
                       TaxiButton(
                         title: 'REGISTER',
                         color: BrandColors.colorGreen,
-                        onPressed: () async {},
+                        onPressed: () async {
+                          //check network availability
+
+                          var connectivityResult =
+                              await Connectivity().checkConnectivity();
+                          if (connectivityResult != ConnectivityResult.mobile &&
+                              connectivityResult != ConnectivityResult.wifi) {
+                            showSnackBar('No internet connectivity', context);
+                            return;
+                          }
+
+                          if (fullNameController.text.length < 3) {
+                            showSnackBar(
+                                'Please provide a valid fullname', context);
+                            return;
+                          }
+
+                          if (phoneController.text.length < 10) {
+                            showSnackBar(
+                                'Please provide a valid phone number', context);
+                            return;
+                          }
+
+                          if (!emailController.text.contains('@')) {
+                            showSnackBar('Please provide a valid email address',
+                                context);
+                            return;
+                          }
+
+                          if (passwordController.text.length < 8) {
+                            showSnackBar(
+                                'password must be at least 8 characters',
+                                context);
+                            return;
+                          }
+
+                          registerUser();
+                        },
                       ),
                     ],
                   ),
@@ -140,6 +235,5 @@ class _RegistrationPageState extends State<RegistrationPage> {
         ),
       ),
     );
-    ;
   }
 }
